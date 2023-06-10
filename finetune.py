29072f657975253681e1e625e05372480a812bfb
EC2 Default User <ec2-user@ip-172-16-41-156.us-east-2.compute.internal>
2023-06-10 00:02:42 +0000

    changes to finetune on V100

--- finetune.py
+++ finetune.py
@@ -269,8 +269,9 @@ def train(
 
     if torch.__version__ >= "2" and sys.platform != "win32":
         model = torch.compile(model)
+    with torch.autocast("cuda"):
+        trainer.train(resume_from_checkpoint=resume_from_checkpoint)
 
-    trainer.train(resume_from_checkpoint=resume_from_checkpoint)
 
     model.save_pretrained(output_dir)
 
